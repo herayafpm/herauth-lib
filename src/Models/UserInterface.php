20fe69ee132697ddcd2742d02bbad77241084b77
heraya <herayafpm@gmail.com>
2022-01-26 11:34:09 +0700

    update user interface

--- src/Models/UserInterface.php
+++ src/Models/UserInterface.php
@@ -2,11 +2,8 @@
 
 namespace Raydragneel\HerauthLib\Models;
 
-use Raydragneel\HerauthLib\Entities\AccountEntity;
-
 interface UserInterface
 {
 
     public function cekUser(string $username);
-    public function attempt(AccountEntity $entity);
 }
