f72149a6bfa57bcc10668af4a2a76d26bc3ef21b
heraya <herayafpm@gmail.com>
2022-01-26 11:44:41 +0700

    update routes

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -18,7 +18,9 @@ $routes->group('herauth',function($routes){
     $routes->namespace = 'Raydragneel\HerauthLib\Controllers';
     $routes->group('', ['namespace' => $routes->namespace], function ($routes) {
         $routes->get('assets/(:any)','Assets::file/$1');
-        $routes->addRedirect('/', 'herauth/id');
+        $routes->get('/', function(){
+            return redirect()->to(herauth_base_locale_url());
+        });
         $routes->group('{locale}', ['namespace' => $routes->namespace,'filter' => 'auth_filter'], function ($routes) {
             $routes->get('logout','Auth::logout');
             $routes->get('login','Auth::login');
